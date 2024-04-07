067fe515ba047db780486ec48da78d33d98fe08c
Es-Frost <natsios_s@hotmail.com>
2024-04-07 23:15:20 +0200

    Design drafting

---
+++ admin-panel/src/main.js
@@ -0,0 +1,3 @@
+const header = document.getElementsByClassName('header')[0]
+const sidebar = document.getElementsByClassName('sidebar')[0]
+const footer = document.getElementsByClassName('footer')[0]
