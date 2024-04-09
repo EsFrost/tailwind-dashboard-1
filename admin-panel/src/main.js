5de188c38661dde789488626170ebe8030ce8e04
EsFrost <sigmafi.tech@gmail.com>
2024-04-09 23:15:19 +0200

    Created welcome card

--- admin-panel/src/main.js
+++ admin-panel/src/main.js
@@ -1,3 +1,4 @@
 const header = document.getElementsByClassName('header')[0]
 const sidebar = document.getElementsByClassName('sidebar')[0]
 const footer = document.getElementsByClassName('footer')[0]
+
