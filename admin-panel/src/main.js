189e0b7426aafb9acc9a4034f5d0d3035aba9134
EsFrost <sigmafi.tech@gmail.com>
2024-04-11 09:25:15 +0200

    New pc

--- admin-panel/src/main.js
+++ admin-panel/src/main.js
@@ -2,3 +2,5 @@ const header = document.getElementsByClassName('header')[0]
 const sidebar = document.getElementsByClassName('sidebar')[0]
 const footer = document.getElementsByClassName('footer')[0]
 
+/*
+*/
